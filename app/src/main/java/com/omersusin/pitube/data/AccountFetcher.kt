8e202f8e87a0dbb5c38c47b1ee4dca1cfff518f2
omersusin <tekgercek73@gmail.com>
2026-08-08 18:19:00 +0300

    Fix compilation errors: MediaType.toMediaType(), add context parameter to StreamResolver.resolve() and all calls

--- app/src/main/java/com/omersusin/pitube/data/AccountFetcher.kt
+++ app/src/main/java/com/omersusin/pitube/data/AccountFetcher.kt
@@ -1,6 +1,7 @@
 package com.omersusin.pitube.data
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaType
 
 import android.content.Context
 import kotlinx.coroutines.Dispatchers
