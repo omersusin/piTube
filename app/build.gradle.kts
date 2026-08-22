d07a760ba79b94307fa299d612c704a592e80ed2
omersusin <tekgercek73@gmail.com>
2026-08-22 22:37:14 +0300

    chore: bump versionCode 19 / 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.omersusin.pitube"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "2.2.1"
+        versionCode = 19
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "com.omersusin.pitube.HiltTestRunner"
         vectorDrawables {
