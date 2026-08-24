256ffcb34e451a9ec3bd9b111ae2dca411592e06
omersusin <tekgercek73@gmail.com>
2026-08-24 05:13:44 +0300

    fix: YouTubeClient has clientName not name (3.0.9/63)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,9 @@ android {
         applicationId = "com.omersusin.pitube"
         minSdk = 26
         targetSdk = 36
-        versionCode = 62
+        versionCode = 63
 
-        versionName = "3.0.8"
+        versionName = "3.0.9"
         testInstrumentationRunner = "com.omersusin.pitube.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
